4178cc4353a82aa4d6e95b5810ebd6e95a140b76
Sheenan Tenepre <sheenantenepre@gmail.com>
2017-04-14 17:47:50 +0900

    fix up faker

--- populate_first_app.py
+++ populate_first_app.py
@@ -1,50 +1,42 @@
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'contempo_python_api.settings')
-
 import django
+from faker import Faker
+import random
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'contempo_python_api.settings')
 django.setup()
 
-## FAKE SCRIPT
-import random
-from first_app.models import AccessRecord,WebPage,Topic,User
-from faker import Faker
+from first_app.models import AccessRecord, WebPage, Topic
+
+# FAKE SCRIPT
 
 fakegen = Faker()
-topics = ['Search', 'Social', 'Marketplace', 'News' , 'Games']
+topics = ['Search', 'Social', 'Marketplace', 'News', 'Games']
+
 
 def add_topic():
-  t = Topic.objects.get_or_create(top_name=random.choice(topics))[0]
-  t.save()
-  return t
-
-def populate(N=5):
-  for entry in range(N):
-    # get topic for the entry
-    top = add_topic()
-
-    # create fake data
-    fake_url = fakegen.url()
-    fake_date = fakegen.date()
-    fake_name = fakegen.company()
-
-    # create new webpage entry
-    webpage = WebPage.objects.get_or_create(topic=top,url=fake_url,name=fake_name)[0]
+    t = Topic.objects.get_or_create(top_name=random.choice(topics))[0]
+    t.save()
+    return t
 
 
-    # create access record
-    acc_rec = AccessRecord.objects.get_or_create(name=webpage,date=fake_date)[0]
+def populate(n=5):
+    for entry in range(n):
+        # get topic for the entry
+        top = add_topic()
 
-def fake_users(N=5):
-  for entry in range(N):
-    fake_first_name = fakegen.name()
-    fake_last_name = fakegen.name()
-    fake_email = fakegen.email()
+        # create fake data
+        fake_url = fakegen.url()
+        fake_date = fakegen.date()
+        fake_name = fakegen.company()
 
-    # create fake user
-    users = User.objects.get_or_create(first_name=fake_first_name,last_name=fake_last_name,email=fake_email)[0]
+        # create new web_page entry
+        web_page = WebPage.objects.get_or_create(topic=top, url=fake_url, name=fake_name)[0]
+
+        # create access record
+        acc_rec = AccessRecord.objects.get_or_create(name=web_page, date=fake_date)[0]
 
 if __name__ == '__main__':
-  print('populating script!')
-  populate(0)
-  fake_users(0)
-  print('population complete')
+    print('populating script!')
+    populate(0)
+    print('population complete')
